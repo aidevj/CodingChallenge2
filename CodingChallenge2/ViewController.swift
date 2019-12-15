5092eb7dfc3d4772db0e1490e3e7b2688d004bd3
Aiden Melendez <aidenjyo@gmail.com>
2019-12-14 20:12:10 -0500

    Initial Commit

---
+++ CodingChallenge2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CodingChallenge2
+//
+//  Created by Consultant on 12/14/19.
+//  Copyright © 2019 MAC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
